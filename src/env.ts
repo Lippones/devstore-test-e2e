288144c41786bb90e7766696daffaedcdf734dd0
Filipe Vieira <filipe.silva@cidadaoon.com>
2024-02-09 16:51:29 -0300

    feat: te env

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,15 @@
 import { z } from 'zod'
+import { createEnv } from '@t3-oss/env-nextjs'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-const parseEnv = envSchema.safeParse(process.env)
-
-if (!parseEnv.success) {
-  console.error(
-    'Invalid environment variables:',
-    parseEnv.error.flatten().fieldErrors,
-  )
-
-  throw new Error('Invalid environment variables')
-}
-
-export const env = parseEnv.data
