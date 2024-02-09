2c2322a231f01e3976b77c59908c8696eabce932
Filipe Vieira <filipe.silva@cidadaoon.com>
2024-02-09 17:06:38 -0300

    fix: change vercel url

--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,13 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    APP_URL: z.string().url(),
+    VERCEL_URL: z.string().url(),
   },
   client: {
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
   },
   runtimeEnv: {
-    APP_URL: process.env.APP_URL,
+    VERCEL_URL: process.env.APP_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 })
